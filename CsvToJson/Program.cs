29cf60098618806c54ab92b7c4af833a7ac2f42c
Konnor Welsch <kwelsch@2ndwatch.com>
2017-07-15 17:49:07 -0700

    Code to open the file that was given as a CL argument

--- CsvToJson/Program.cs
+++ CsvToJson/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
 
 namespace CsvToJson
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string csvPath = CheckArgs(args);
+
+            using (FileStream fileStream = File.Open(csvPath, FileMode.Open, FileAccess.Read))
+            {
+
+            }
+        }
+
+        public static string CheckArgs(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("No arguments given. Exiting");
+                Environment.Exit(1);
+            }
+
+            if(args[0].Substring(args[0].Length - 4) != ".csv")
+            {
+                Console.WriteLine("Given argument not a CSV file. Exiting");
+                Environment.Exit(2);
+            }
+
+            if(!File.Exists(args[0]))
+            {
+                Console.WriteLine("Given file not found. Exiting");
+                Environment.Exit(1);
+            }
+
+            return args[0];
         }
     }
 }
